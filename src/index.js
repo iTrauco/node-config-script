a59b6581e3a937097b41359c766c137810c1da70
Christopher Trauco <Chris@Trau.co>
2019-09-12 16:28:37 -0400

    default to current directory name

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
+const path = require('path');
 const  inquirer = require('inquirer');
 
 const existingConfig = fs.existsSync('now.json');
+
+// console.log(__dirname);
+// console.log(process.cwd());
 
 function buildConfig() {
     inquirer
     .prompt([
         {
-            type: 'confirm',
-            name: 'overwrite',
-            message: '\'now.json\' already exists! Do you want to overwrite the existing file?',
-            default: false
+            type: 'text',
+            name: 'name',
+            message: 'What is the name of the project?',
+            default: path.basename(process.cwd())
         }
     ])
     .then(answers => {
-        if(answers.overwrite) {
-            buildConfig();
-        } else {
-            console.log('Goodbye...');
-        }
+        console.log(answers);
+        // if(answers.overwrite) {
+        //     buildConfig();
+        // } else {
+        //     console.log('Goodbye...');
+        // }
     });
 }
 
