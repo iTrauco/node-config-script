b2512a9173df3f187b392c828af341fb2c39690a
Christopher Trauco <Chris@Trau.co>
2019-09-12 13:30:35 -0400

    correcting double promises

---
+++ README.md
@@ -0,0 +1,18 @@
+#node-config-script
+
+Node.js CLI script for now.sh config file generation/creation. 
+
+### Install
+
+```
+npm install -g now-config
+```
+
+### Usage
+```
+now-config
+```
+
+### Example/Preview
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Work in progress...');
+const fs = require('fs');
+const  inquirer = require('inquirer');
+
+const existingConfig = fs existsSync('now.json');
+
+const questions  = [];
+
+if (existingConfig) {
+    inquirer
+    prompt([
+        {
+            type: 'confirm',
+            name: 'overwrite',
+            message: '\'now.json\' already exists! Do you want to overwrite the existing file?',
+            default: false
+        }
+    ])
+    .then(answers => {
+        if(answers.overwrite) {
+
+        }
+    });
+} else {
+    
+}
+
+
+
+// inquirer
+//   .prompt([
+//     {
+//         type: 'confirm',
+//         name: 'isRobot',
+//         message: 'Are you a robot?',
+//         default: false
+//       },
+//   ])
+//   .then(answers => {
+//     console.log(answers);
+//   });
